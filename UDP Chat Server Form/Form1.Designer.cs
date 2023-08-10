937dfcdec96945a33d9ebcda99c2fecb92c945a9
Halil İbrahim Toprak <72528672+lasthit99@users.noreply.github.com>
2023-08-10 19:08:27 +0300

    UDP Socket Programming-Chat Application
    UDP Socket Programming-Chat Application with C#.Net

---
+++ UDP Chat Server Form/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace UDP_Chat_Server_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbConsole = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbConsole
+            // 
+            this.tbConsole.Location = new System.Drawing.Point(15, 30);
+            this.tbConsole.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbConsole.Multiline = true;
+            this.tbConsole.Name = "tbConsole";
+            this.tbConsole.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbConsole.Size = new System.Drawing.Size(1035, 509);
+            this.tbConsole.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "-- >>";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbConsole);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "UDP Asenkron Chat";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbConsole;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ UDP Chat Server Form/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UDP_Asynchronous_Chat;
+
+namespace UDP_Chat_Server_Form
+{
+    public partial class Form1 : Form
+    {
+        UDP_Asynchronous_Chat.UDPAsynchronousChatServer mUDPChatServer; 
+
+        
+        public Form1()
+        {
+            mUDPChatServer = new UDP_Asynchronous_Chat.UDPAsynchronousChatServer();
+            mUDPChatServer.RaisePrintStringEvent += chatClient_PrintString;
+            InitializeComponent();
+        }
+
+        private void chatClient_PrintString(object sender, PrintStringEventArgs e)
+        {
+            Action<string> print = PrintToTextBox;
+
+            tbConsole.Invoke(print, new string[] { e.MessageToPrint });
+
+        }
+
+        private void PrintToTextBox(string stringToPrint)
+        {
+            tbConsole.Text += $"{Environment.NewLine}{DateTime.Now} - {stringToPrint}";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mUDPChatServer.StartReceivingData();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
